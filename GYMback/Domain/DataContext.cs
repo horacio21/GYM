0bc33b5c58093d93f93ba14d051fa71fa505e49e
horacioud <20121346@ce.pucmm.edu.do>
2017-05-23 00:15:05 -0400

    DataContext

---
+++ GYMback/Domain/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace Domain
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ GYMback/Domain/Rutina.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class Rutina
+    {
+        [Key]
+        public int RutinaId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MaxLength(50, ErrorMessage = "The Minimun Length for field {0} is {1} characters")]
+        [Index("Rutina_Nombre_Index", IsUnique = true)]
+        [Display(Name = "Rutina")]
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
